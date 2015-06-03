6d7322337f52b02567b0b7de810fdb1e2219b8ca
Oliver Ochs <oliver.ochs@holisticon.de>
2015-06-03 09:56:25 +0200

    ????

--- GetData/week1/question5.R
+++ GetData/week1/question5.R
@@ -1,6 +1,6 @@
 
 fileName <- "getdata-fsspid.csv"
-if (file.exists(fileName)) {
+if (!file.exists(fileName)) {
   fileUrl1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
   download.file(fileUrl, destfile=fileName, method="curl", mode="w");
   dateDownloaded <- date();
